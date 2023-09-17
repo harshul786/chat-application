43e32664532921449b3aa50f5e65d6e6ffc936fd
harshul786 <harshul.n786@gmail.com>
2023-09-17 14:32:10 +0530

    Trying to fix err 3

---
+++ src/Components/ChatView/index.jsx
@@ -0,0 +1,100 @@
+import { BsFillCaretLeftFill } from "react-icons/bs";
+import DefaultProfile from "../DefaultProfile";
+import { ChatState } from "../../Context/chatProvider";
+import React, { useEffect, useRef } from "react";
+
+const YouMessage = ({ messages }) => {
+  return (
+    <div className="my-2 ml-10 flex flex-col gap-2">
+      {messages.map((message, index) => (
+        <div className={"relative"} key={index}>
+          {index === messages.length - 1 ? (
+            <>
+              <div className="absolute z-10 -left-10 bottom-1 h-max">
+                {message.sender.avatar ? (
+                  <div className="w-[28px] h-[28px] overflow-hidden rounded-full relative">
+                    <img
+                      src={message.sender.avatar}
+                      alt="avatar"
+                      className="h-full w-auto object-cover absolute top-0 left-1/2 -translate-x-1/2"
+                    />
+                  </div>
+                ) : (
+                  <DefaultProfile height="28px" width="28px" size="20" />
+                )}
+              </div>
+
+              <BsFillCaretLeftFill
+                className={`absolute -left-[11px] bottom-0.5 text-gray-100`}
+                size={20}
+              />
+            </>
+          ) : (
+            ""
+          )}
+
+          <div className="bg-gray-100 py-2 px-4 rounded-md max-w-xs">
+            {message.content}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const MeMessage = ({ messages }) => {
+  return (
+    <div className="my-2 flex flex-col gap-2 items-end">
+      {messages.map((message, index) => (
+        <div className={"relative"} key={index}>
+          {index === messages.length - 1 ? (
+            <BsFillCaretLeftFill
+              className={`rotate-180 absolute -right-[11px] bottom-0.5 text-blue-500`}
+              size={20}
+            />
+          ) : (
+            ""
+          )}
+          <div className="bg-blue-500 text-white py-2 px-4 rounded-md max-w-xs">
+            {message.content}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default function ChatView({ groupedMessages }) {
+  const { user } = ChatState();
+  const chatBottomRef = useRef(null);
+
+  useEffect(() => {
+    if (chatBottomRef.current) {
+      console.log("scroll");
+      chatBottomRef.current.scrollIntoView({
+        // behavior: "smooth",
+        block: "end",
+      });
+    }
+  }, [
+    groupedMessages.length,
+    groupedMessages[groupedMessages.length - 1]?.length,
+  ]);
+
+  return (
+    <div className="flex-1 overflow-y-scroll px-4 py-2 smooth-scroll">
+      {/* Render grouped chat messages */}
+      {groupedMessages.map((messageGroup, groupIndex) => {
+        const senderId = messageGroup[0].sender._id;
+
+        return senderId === user._id ? (
+          <MeMessage key={groupIndex} messages={messageGroup} />
+        ) : (
+          <YouMessage key={groupIndex} messages={messageGroup} />
+        );
+      })}
+
+      {groupedMessages.length !== 0 && <div className="" ref={chatBottomRef} />}
+    </div>
+  );
+}
--- src/Components/SingleChat/index.jsx
+++ src/Components/SingleChat/index.jsx
@@ -1,72 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
 import { groupConsecutiveMessages } from "../../utils/utils";
 import { ChatState } from "../../Context/chatProvider";
-import { BsFillCaretLeftFill } from "react-icons/bs";
-import DefaultProfile from "../DefaultProfile";
 import Loading from "../Loading";
 import io from "socket.io-client";
-var socket, selectedChatCompare;
-
-const YouMessage = ({ messages }) => {
-  return (
-    <div className="my-2 ml-10 flex flex-col gap-2">
-      {messages.map((message, index) => (
-        <div className={"relative"} key={index}>
-          {index === messages.length - 1 ? (
-            <>
-              <div className="absolute z-10 -left-10 bottom-1 h-max">
-                {message.sender.avatar ? (
-                  <div className="w-[28px] h-[28px] overflow-hidden rounded-full relative">
-                    <img
-                      src={message.sender.avatar}
-                      alt="avatar"
-                      className="h-full w-auto object-cover absolute top-0 left-1/2 -translate-x-1/2"
-                    />
-                  </div>
-                ) : (
-                  <DefaultProfile height="28px" width="28px" size="20" />
-                )}
-              </div>
-
-              <BsFillCaretLeftFill
-                className={`absolute -left-[11px] bottom-0.5 text-gray-100`}
-                size={20}
-              />
-            </>
-          ) : (
-            ""
-          )}
-
-          <div className="bg-gray-100 py-2 px-4 rounded-md max-w-xs">
-            {message.content}
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-const MeMessage = ({ messages }) => {
-  return (
-    <div className="my-2 flex flex-col gap-2 items-end">
-      {messages.map((message, index) => (
-        <div className={"relative"} key={index}>
-          {index === messages.length - 1 ? (
-            <BsFillCaretLeftFill
-              className={`rotate-180 absolute -right-[11px] bottom-0.5 text-blue-500`}
-              size={20}
-            />
-          ) : (
-            ""
-          )}
-          <div className="bg-blue-500 text-white py-2 px-4 rounded-md max-w-xs">
-            {message.content}
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
+import ChatView from "../ChatView";
+var socket;
 
 export default function SingleChat({ chatId }) {
   const [messages, setMessages] = useState([]);
@@ -80,39 +18,6 @@ export default function SingleChat({ chatId }) {
   const [typing, setTyping] = useState(false);
   const [isTyping, setIsTyping] = useState(false);
   const [whoTyping, setWhoTyping] = useState(null);
-
-  function Chats() {
-    const { user } = ChatState();
-    const chatBottomRef = useRef(null);
-
-    useEffect(() => {
-      if (chatBottomRef.current && groupedMessages.length !== 0) {
-        chatBottomRef.current.scrollIntoView({
-          // behavior: "smooth",
-          block: "end",
-        });
-      }
-    }, [messages.length]);
-
-    return (
-      <div className="flex-1 overflow-y-scroll px-4 py-2 smooth-scroll">
-        {/* Render grouped chat messages */}
-        {groupedMessages.map((messageGroup, groupIndex) => {
-          const senderId = messageGroup[0].sender._id;
-
-          return senderId === user._id ? (
-            <MeMessage key={groupIndex} messages={messageGroup} />
-          ) : (
-            <YouMessage key={groupIndex} messages={messageGroup} />
-          );
-        })}
-
-        {groupedMessages.length !== 0 && (
-          <div className="" ref={chatBottomRef} />
-        )}
-      </div>
-    );
-  }
 
   useEffect(() => {
     socket = io();
@@ -251,9 +156,10 @@ export default function SingleChat({ chatId }) {
       <div className="flex flex-col w-full md:h-screen h-[90vh]">
         <div className="h-16 relative flex flex-col justify-center bg-blue-500 text-white items-left px-4">
           <div
-            className={`text-base absolute transition-all  ${
+            className={`text-base absolute transition-all ${
               isTyping ? "top-3" : "top-1/2 -translate-y-1/2"
-            }`}
+            }
+            `}
           >
             {chatName ? chatName : ""}
           </div>
@@ -272,7 +178,7 @@ export default function SingleChat({ chatId }) {
           </div>
         </div>
 
-        <Chats />
+        <ChatView groupedMessages={groupedMessages} />
 
         <div className="h-14 bg-white dark:bg-gray-950 border-t border-gray-200 flex items-center px-4">
           <input
